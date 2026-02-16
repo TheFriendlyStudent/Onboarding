f577d242c74a59c5ffab771e3b4cc089fd2e5cb0
Rishi Boggavarapu <83251988+TheFriendlyStudent@users.noreply.github.com>
2026-02-16 10:47:51 -0600

    More comments

--- onboarding.py
+++ onboarding.py
@@ -1,14 +1,13 @@
 # DFR Onboarding Project
 
 from dash import Dash, dcc, html, Input, Output
+from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 
 #import the data and drop columns with only one unique value to clean up data
 data = pd.read_csv('can_data.csv', on_bad_lines='skip')
-
-print(data.head(10000))
 
 for each in data.columns:
     data[each] = data[each].dropna()
@@ -19,23 +18,22 @@ for each in data.columns:
 data = data.dropna(how="any")
 data["timestamp"] = pd.to_datetime(data["timestamp"], unit="s")
 
+#smoothing the speed data for better readability 
+#calculating acceleration by taking derivative of speed over time
 speed_converted = data["Driven Avg Wheel Speed"] / 3.6
-speed_mps_smooth = speed_converted.rolling(10, center=True).mean()
+speed_mps_smooth = speed_converted.rolling(25, center=True).mean()
+
+#adding the new values to the dataframe
 data["Accel"] = speed_mps_smooth.diff() / data["timestamp"].diff().dt.total_seconds()
 
 #setting up the plotly graph and dash app to display data
 app = Dash(__name__)
 
-
+#creating dropdowns to toggle visibility of the two graphs
 app.layout = html.Div([
     html.H4('Data Visualization: DFR Onboarding Project'),
     html.P("Select color:"),
-    dcc.Dropdown(
-        id="dropdown",
-        options=['Gold', 'MediumTurquoise', 'LightGreen'],
-        value='Gold',
-        clearable=False,
-    ),html.P("Show Acceleration:"),
+    html.P("Show Acceleration:"),
     dcc.Dropdown(
         id="dropdown2",
         options=[True, False],
@@ -52,35 +50,46 @@ app.layout = html.Div([
 ])
 
 #produce two graphs plotting two inputs over time
+#runtime code to update the graph based on dropdown selections
 @app.callback(
-    Output("graph", "figure"),
-    Input("dropdown", "value"),
-    Input("dropdown2", "value"),
-    Input("dropdown3", "value"))
-def display_color(color, show_accel, show_analog2):
-    fig = go.Figure()
+    Output("graph", "figure"))
+def display_color():
 
-    fig.add_trace(go.Scatter(
-        x=data["timestamp"],
-        y=data["Analog Input #2"],
-        mode="lines",
-        name="Analog Input #2",
-        line=dict(color=color),
-        visible=show_analog2))
+    fig = make_subplots(
+        rows=2,
+        cols=1,
+        shared_xaxes=True,
+        subplot_titles=("Analog Input #2", "Acceleration")
+    )
 
-    fig.add_trace(go.Scatter(
-        x=data["timestamp"],
-        y=data["Accel"],
-        mode="lines",
-        name="Acceleration",
-        visible=show_accel
-    ))
+    # Top chart
+    fig.add_trace(
+        go.Scatter(
+            x=data["TPS"],
+            y=data["Fuel Open Time"],
+            mode="lines",
+            name="Analog Input #2"
+        ),
+        row=1,
+        col=1
+    )
+
+    # Bottom chart
+    fig.add_trace(
+        go.Scatter(
+            x=data["timestamp"],
+            y=data["Accel"],
+            mode="lines",
+            name="Acceleration"
+        ),
+        row=2,
+        col=1
+    )
 
     fig.update_layout(
+        height=700,
         title="Acceleration vs Time",
-        xaxis_title="Time",
-        yaxis_title="Value",
-        legend_title="Signals"
+        showlegend=True
     )
 
     return fig
